ddf43432274c602a28a86ee7464bc78365befc8a
chuachuachuacher <15281139469@163.com>
2024-09-16 19:51:08 +0800

    first testing creeper for the 250Top rank of douban

---
+++ Top250Creeper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 构造请求头、get网页源码
+MyHeaders={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0"
+}
+num = 0
+for i in range(0, 250, 25):
+    response = requests.get("https://movie.douban.com/top250?start={}&filter=".format(i), headers=MyHeaders)
+    # 处理源码
+    if response.ok:
+        content = response.text
+        soup = BeautifulSoup(content, "html.parser")
+        titles = soup.findAll("span", {"class": "title"})
+        for title in titles:
+            if '/' not in title.string:
+                num += 1
+                print("电影号{}: ".format(num),title.string)
+    else:
+        print('the method of get Failed!/n')
+
+response.close()
